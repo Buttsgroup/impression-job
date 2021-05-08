9a1392e481324010f3810d82b8e5156652c8a4ff
Max Williams <me@maxwillia.ms>
2021-05-08 16:36:53 +0100

    - update setup.py - add licence

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ Allows package to be installed in "editable" mode by running `pip install -e`
 Can change source code and rerun tests at will
 """
 from setuptools import setup, find_packages
-from impression_job import __version__
+import impression_job
 
+# get the dependencies and installs
+with open('requirements.txt', 'r') as f:
+    requires = [x.strip() for x in f if x.strip()]
 
-setup(name="impression_job", packages=find_packages(), version=__version__)
+setup(
+    name='impression_job',
+    packages=find_packages(),
+    version=impression_job.__version__,
+    author=impression_job.__author__,
+    author_email=impression_job.__author_email__,
+    license=impression_job.__licence__,
+    install_requires=requires)
